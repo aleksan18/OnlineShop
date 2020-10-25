74802fbed87a486f5fe30a7fc78be6a0686de7cc
Alexander Minchev <aleksander.minchev18@gmail.com>
2020-10-25 10:34:45 +0100

    Models for items and sales

---
+++ src/main/java/com/example/demo/Model/Items.java
@@ -0,0 +1,141 @@
+package com.example.demo.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Items {
+    @Id
+    private int Id;
+    private int sales_id;
+    private String size;
+    private double price;
+    private String name;
+    private int stock;
+    private String type;
+    private int ratings;
+    private int quantity;
+
+    public Items() {
+    }
+
+    public Items(int id, int sales_id, String size, double price, String name, int stock, String type, int ratings, int quantity) {
+        Id = id;
+        this.sales_id = sales_id;
+        this.size = size;
+        this.price = price;
+        this.name = name;
+        this.stock = stock;
+        this.type = type;
+        this.ratings = ratings;
+        this.quantity = quantity;
+    }
+
+    public int getSales_id() {
+        return sales_id;
+    }
+
+    public void setSales_id(int sales_id) {
+        this.sales_id = sales_id;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Items items = (Items) o;
+        return Id == items.Id &&
+                Double.compare(items.price, price) == 0 &&
+                stock == items.stock &&
+                ratings == items.ratings &&
+                quantity == items.quantity &&
+                Objects.equals(size, items.size) &&
+                Objects.equals(name, items.name) &&
+                Objects.equals(type, items.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, size, price, name, stock, type, ratings, quantity);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(int ratings) {
+        this.ratings = ratings;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Items{" +
+                "Id=" + Id +
+                ", sales_id=" + sales_id +
+                ", size='" + size + '\'' +
+                ", price=" + price +
+                ", name='" + name + '\'' +
+                ", stock=" + stock +
+                ", type='" + type + '\'' +
+                ", ratings=" + ratings +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/demo/Model/Sales.java
@@ -0,0 +1,82 @@
+package com.example.demo.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Sales {
+    @Id
+    private int Id;
+    private boolean completed;
+    private int customers_id;
+    private double final_price;
+
+    public Sales() {
+    }
+
+    public Sales(int id, boolean completed, int customers_id, double final_price) {
+        Id = id;
+        this.completed = completed;
+        this.customers_id = customers_id;
+        this.final_price = final_price;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public int getCustomers_id() {
+        return customers_id;
+    }
+
+    public void setCustomers_id(int customers_id) {
+        this.customers_id = customers_id;
+    }
+
+    public double getFinal_price() {
+        return final_price;
+    }
+
+    public void setFinal_price(double final_price) {
+        this.final_price = final_price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sales sales = (Sales) o;
+        return Id == sales.Id &&
+                completed == sales.completed &&
+                customers_id == sales.customers_id &&
+                Double.compare(sales.final_price, final_price) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, completed, customers_id, final_price);
+    }
+
+    @Override
+    public String toString() {
+        return "Sales{" +
+                "Id=" + Id +
+                ", completed=" + completed +
+                ", customers_id=" + customers_id +
+                ", final_price=" + final_price +
+                '}';
+    }
+}
